8e2539f1aa475270cd2d73b356daf1fb5000625c
mee-lan <milan.gyawali404@gmail.com>
2026-03-27 11:13:11 +0545

    style: fixed search icon overlapping with text

--- src/pages/teacher/TeacherStudents.tsx
+++ src/pages/teacher/TeacherStudents.tsx
@@ -75,7 +75,7 @@ export default function TeacherStudents() {
         </div>
         
         <div className="relative">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
             <svg className="h-5 w-5 text-neutral-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
